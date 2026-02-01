79c8abdfb9bc78627d196325849e0fa39c10d44f
CristianCastiglione <cristian_castiglione@libero.it>
2026-02-01 14:35:09 +0100

    New simulation experiments

--- sgdGMF_manuscript.R
+++ sgdGMF_manuscript.R
@@ -1,69 +1,75 @@
 # ----------- Simulation benchmark
 
+path_sim <- "Benchmarking/Simulation"
+
 # First, all data is simulated with splatter, and all methods are run on these
 # simulated datasets.
-source("Benchmarking/Simulation/simulation_MAIN.R")
-source("Benchmarking/Simulation/simulation_NB.R")
+source(paste(path_sim, "simulation_MAIN.R", sep="/"))
+source(paste(path_sim, "simulation_NB.R", sep="/"))
 
 # Then, figures are made for the summary statistics and the tSNE projections.
-source("Benchmarking/Simulation/plot_sim_example_MAIN.R")
+source(paste(path_sim, "plot_sim_example_MAIN.R", sep="/"))
 
 # Similarly, figures are made for the summary statistics in function of the
 # matrix dimensions and latent space rank.
-source("Benchmarking/Simulation/plot_sim_summary_MAIN.R")
-source("Benchmarking/Simulation/plot_sim_summary_NB.R")
+source(paste(path_sim, "plot_sim_summary_MAIN.R", sim="/"))
+source(paste(path_sim, "plot_sim_summary_NB.R", sim="/"))
 
 
 # ----------- Arigoni benchmark
 
+path_arigoni <- "Benchmarking/Arigoni"
+
 # First, the raw data of all celltypes is merged together in one file, and
 # an initial quality control is done.
-source("Benchmarking/Arigoni/preprocessing.R")
+source(paste(path_arigoni, "preprocessing.R", sep="/"))
 
 # Then, the data is additionally filtered. For example, PBMC cells are not used
 # and cells with many mitochondrial reads are removed.
-source("Benchmarking/Arigoni/Data_preparation.R")
+source(paste(path_arigoni, "Data_preparation.R", sep="/"))
 
 # We calculate a scree-plot based on the eigenvalues for the model selection.
-source("Benchmarking/Arigoni/Eigenvalues_model_selection.R")
+source(paste(path_arigoni, "Eigenvalues_model_selection.R", sep="/"))
 
 # We also perform cross-validation based on model selection criteria and
 # out of sample deviances to select the number of latent factors.
-source("Benchmarking/Arigoni/sgdGMF-cv.R")
+source(paste(path_arigoni, "sgdGMF-cv.R", sep="/"))
 
 # sgdGMF estimation is performed using the optimal number of latent factors
 # chosen in the cross-validation.
-source("Benchmarking/Arigoni/sgdGMF-fitting.R")
+source(paste(path_arigoni, "sgdGMF-fitting.R", sep="/"))
 
 # Benchmarking with glmPCA and NewWave is done. Here, these models are
 # used to perform dimensionality reduction on the Arigoni dataset.
-source("Benchmarking/Arigoni/NewWave_comparison.R")
+source(paste(path_arigoni, "NewWave_comparison.R", sep="/"))
 
 # We here group all results in data.frames that can be used to make the plots
 # of the manuscript.
-source("Benchmarking/Arigoni/Figure_preparation.R")
+source(paste(path_arigoni, "Figure_preparation.R", sep="/"))
 
 # We here construct the plots of the manuscript.
-source("Benchmarking/Arigoni/Figures_construction.R")
+source(paste(path_arigoni, "Figures_construction.R", sep="/"))
 
 
 
 # ----------- CaseStudy benchmark
 
+path_casestudy <- "Benchmarking/CaseStudy"
+
 # First, the dataset is downloaded and quality control is performed to
 # remove aberrant samples or genes.
-source("Benchmarking/CaseStudy/TENxBrainData_Processing.R")
+source(paste(path_casestudy, "TENxBrainData_Processing.R", sep="/"))
 
 # The variance of each gene is estimated to select the most variable
 # genes of the dataset. These genes will be further used in the decomposition.
-source("Benchmarking/CaseStudy/High_variable_genes.R")
+source(paste(path_casestudy, "High_variable_genes.R", sep="/"))
 
 # We calculate a scree-plot based on the eigenvalues for the model selection.
-source("Benchmarking/CaseStudy/Model_selection_eigenvalues.R")
+source(paste(path_casestudy, "Model_selection_eigenvalues.R", sep="/"))
 
 # sgdGMF estimation is performed using the optimal number of latent factors
 # estimated with the scree plot.
-source("Benchmarking/CaseStudy/CaseStudy_Model_fitting_full.R")
+source(paste(path_casestudy, "CaseStudy_Model_fitting_full.R", sep="/"))
 
 # For the comparisons of the time-usage, sgdGMF is also run on smaller
 # parts of the data. Here, 5 times, the time is measured to compute the
@@ -73,39 +79,39 @@ source("Benchmarking/CaseStudy/CaseStudy_Model_fitting_full.R")
 # NewWave couldn't be run 5 times within the limited computing time (72 hours),
 # it was run in 5 different scripts for each subsampled dataset.
 
-source("Benchmarking/CaseStudy/CaseStudy_sgdGMF_100k_1000_1000_250.R")
-source("Benchmarking/CaseStudy/CaseStudy_sgdGMF_200k_2000_1000_250.R")
-source("Benchmarking/CaseStudy/CaseStudy_sgdGMF_300k_3000_1000_250.R")
-source("Benchmarking/CaseStudy/CaseStudy_glmpca_100k_avagrad_def.R")
-source("Benchmarking/CaseStudy/CaseStudy_glmpca_200k_avagrad_def.R")
-source("Benchmarking/CaseStudy/CaseStudy_glmpca_300k_avagrad_def.R")
-source("Benchmarking/CaseStudy/CaseStudy_glmpca_100k_fisher_def.R")
-source("Benchmarking/CaseStudy/CaseStudy_glmpca_200k_fisher_def.R")
-source("Benchmarking/CaseStudy/CaseStudy_glmpca_300k_fisher_def.R")
+source(paste(path_casestudy, "CaseStudy_sgdGMF_100k_1000_1000_250.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_sgdGMF_200k_2000_1000_250.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_sgdGMF_300k_3000_1000_250.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_glmpca_100k_avagrad_def.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_glmpca_200k_avagrad_def.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_glmpca_300k_avagrad_def.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_glmpca_100k_fisher_def.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_glmpca_200k_fisher_def.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_glmpca_300k_fisher_def.R", sep="/"))
 
 
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_100k_def.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_100k_def_2.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_100k_def_3.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_100k_def_4.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_100k_def_5.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_200k_def.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_200k_def_2.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_200k_def_3.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_200k_def_4.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_200k_def_5.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_300k_def.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_300k_def_2.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_300k_def_3.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_300k_def_4.R")
-source("Benchmarking/CaseStudy/CaseStudy_NewWave_300k_def_5.R")
+source(paste(path_casestudy, "CaseStudy_NewWave_100k_def.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_100k_def_2.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_100k_def_3.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_100k_def_4.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_100k_def_5.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_200k_def.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_200k_def_2.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_200k_def_3.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_200k_def_4.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_200k_def_5.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_300k_def.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_300k_def_2.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_300k_def_3.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_300k_def_4.R", sep="/"))
+source(paste(path_casestudy, "CaseStudy_NewWave_300k_def_5.R", sep="/"))
 
 
 # This script computes the heatmap plot of the manuscript.
-source("Benchmarking/CaseStudy/Heatmap_plot.R")
+source(paste(path_casestudy, "Heatmap_plot.R", sep="/"))
 
 # This script computes the time plot of the manuscript.
-source("Benchmarking/CaseStudy/time_plots.R")
+source(paste(path_casestudy, "time_plots.R", sep="/"))
 
 # Here, both figures are merged into one figure for the manuscript.
-source("Benchmarking/CaseStudy/CaseStudy_merging_plots.R")
+source(paste(path_casestudy, "CaseStudy_merging_plots.R", sep="/"))
